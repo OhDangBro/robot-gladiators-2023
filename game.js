6214ca07105ac14cbde0cd5ad3f6856b4bbf0ddb
William Gehrke <williamegehrke@gmail.com>
2022-12-26 18:59:35 -0500

    add enemy combatants

--- game.js
+++ game.js
@@ -1,25 +1,16 @@
 const playerName = "Bill";
-
 var playerMoney = 10;
-var playerHealth = 100;
-// check to see if the value of the playerHealth variable is greater than 0
-if (playerHealth > 0) {
-  console.log("Your player is still alive!");
-}
-
-
-
 var playerAttack = 10;
+var playerHealth = 100;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-var fight = function () {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -80,4 +71,6 @@ else {
 
 
 // execute function
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+ fight(enemyNames[i])
+}
